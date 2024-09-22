e5cd04e1eacc245ef2ffb6ec6bfbfc5b45ae81ce
Gerardo Diaz <gerardoisai.diaz@gmail.com>
2024-09-22 07:46:59 -0600

    Update SES policy

--- infra/iam.tf
+++ infra/iam.tf
@@ -86,7 +86,7 @@ resource "aws_iam_policy" "cngrs_api_instance_policy" {
         Action = [
           "s3:PutObject",
           "s3:DeleteObject",
-          "ses:SendEmail"
+          "ses:SendRawEmail"
         ]
         Effect   = "Allow"
         Resource = "${aws_s3_bucket.cngrs_bucket.arn}/*"
